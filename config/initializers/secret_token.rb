d3c8b58514c5b6ff567b30ab51dadf855c301c70
Boris Staal <boris@roundlake.ru>
2012-11-12 20:51:00 +0700

    Inited

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BankrazvitieRu::Application.config.secret_token = '4bbf49d4102eeb659fb331d70014de363b6359f643c01209a6e30b7fa33d5e62fe2f8ba6de043a6b5a7fc4fc3020a2fadacf295827d03963181cce4f9b59cb74'
